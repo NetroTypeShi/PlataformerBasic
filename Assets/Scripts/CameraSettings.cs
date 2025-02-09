ecd95e2c106f30a82cb0e2e03964135cde8f6675
NetroTypeShi <andresmenduina@gmail.com>
2025-02-09 17:55:12 +0100

    yeah
    sajdjaksdha

---
+++ Assets/Scripts/CameraSettings.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraSettings : MonoBehaviour
+{
+    [SerializeField] public Transform target;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -1));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+    }
+}
